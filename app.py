daff63b14095b3418f67632db60ca4238273e0cd
Danilo <danilofelix827@gmail.com>
2025-03-05 11:42:26 -0300

    Atualizando projeto para hospedar no vercel

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 from routes.chat import socketio, init_app, bp_chat
 from routes.register import bp_register
 
-# Cria uma instância do Flask
+
 app = Flask(__name__)
 app.secret_key = 'jdefnbys@J#'
 
-# Inicializa o SocketIO com o aplicativo Flask
+
 init_app(app)
 
 
